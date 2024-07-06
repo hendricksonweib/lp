aa0ada1c70a9a0d52060f1bd697678237c19978e
hendricksonweib <hendricksonrocha22@gmail.com>
2024-07-06 12:37:41 -0300

    feat: translater FAQ
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- src/components/template/pagesEN/Faq.jsx
+++ src/components/template/pagesEN/Faq.jsx
@@ -6,49 +6,50 @@ import H1 from '../../layout/H1-title'
 function Faq() {
   return (
     <main className='accord'>
-      <Flex item={'center'} gaps={'24px'}>
-        <H1 Title='Perguntas Frequentes' color='white' />
-        <section className='FAQ-conteiner'>
-          <Accordion defaultActiveKey="0" >
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>O que é a Autoclipper e como ela pode ajudar na entrega de conteúdo?</Accordion.Header>
-              <Accordion.Body>
-                A Autoclipper é uma solução que impulsiona a entrega de conteúdo, permitindo que você crie uma máquina de cortes automática para o YouTube. Isso significa que você pode reutilizar horas de vídeos gravados, transformando-os em novos formatos rapidamente. Ela também ajuda a crescer sua estratégia de conteúdo, seguindo os passos dos maiores canais de entretenimento, notícias e educação. Agende uma demonstração para saber mais!
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Quais são os principais benefícios de usar a Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                ● Escolha os melhores clipes de para o seu conteúdo de forma facilitada.
-                ● Edite e legende de forma automática.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header>Para quem é destinado o serviço da Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                ● Rádios Web
-                ● TVs Online
-                ● Podcasts
-                ● Criadores de conteúdo
-                Se você faz parte de algum desses grupos, agende uma demonstração conosco!s
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header>Quais resultados posso esperar ao usar a Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                ● Edite clipes 10 x mais rápidos, utilizando a nossa Inteligência Artificial.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="4">
-              <Accordion.Header>Quanto custa os serviços da Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                Atualmente estamos em teste alpha, disponibilizamos a autoclipper apenas para parceiros qualificados. Faça a sua inscrição e participe da lista de usuários alpha.
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </section>
-      </Flex>
-    </main>
+    <Flex item={'center'} gaps={'24px'}>
+      <H1 Title='Frequently Asked Questions' color='white' />
+      <section className='FAQ-conteiner'>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>What is Autoclipper and how can it help with content delivery?</Accordion.Header>
+            <Accordion.Body>
+              Autoclipper is a solution that boosts content delivery, allowing you to create an automatic clipping machine for YouTube. This means you can reuse hours of recorded videos, transforming them into new formats quickly. It also helps to grow your content strategy, following in the footsteps of the biggest entertainment, news, and education channels. Schedule a demo to learn more!
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>What are the main benefits of using Autoclipper?</Accordion.Header>
+            <Accordion.Body>
+              ● Easily select the best clips for your content.<br />
+              ● Automatically edit and caption.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Who is Autoclipper's service intended for?</Accordion.Header>
+            <Accordion.Body>
+              ● Web Radios<br />
+              ● Online TVs<br />
+              ● Podcasts<br />
+              ● Content creators<br />
+              If you are part of any of these groups, schedule a demo with us!
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>What results can I expect from using Autoclipper?</Accordion.Header>
+            <Accordion.Body>
+              ● Edit clips 10 times faster using our Artificial Intelligence.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>How much do Autoclipper's services cost?</Accordion.Header>
+            <Accordion.Body>
+              We are currently in alpha testing and offer Autoclipper only to qualified partners. Sign up and join the list of alpha users.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </Flex>
+  </main>
+  
   );
 }
 
--- src/components/template/pagesES/Faq.jsx
+++ src/components/template/pagesES/Faq.jsx
@@ -6,49 +6,50 @@ import H1 from '../../layout/H1-title'
 function Faq() {
   return (
     <main className='accord'>
-      <Flex item={'center'} gaps={'24px'}>
-        <H1 Title='Perguntas Frequentes' color='white' />
-        <section className='FAQ-conteiner'>
-          <Accordion defaultActiveKey="0" >
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>O que é a Autoclipper e como ela pode ajudar na entrega de conteúdo?</Accordion.Header>
-              <Accordion.Body>
-                A Autoclipper é uma solução que impulsiona a entrega de conteúdo, permitindo que você crie uma máquina de cortes automática para o YouTube. Isso significa que você pode reutilizar horas de vídeos gravados, transformando-os em novos formatos rapidamente. Ela também ajuda a crescer sua estratégia de conteúdo, seguindo os passos dos maiores canais de entretenimento, notícias e educação. Agende uma demonstração para saber mais!
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Quais são os principais benefícios de usar a Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                ● Escolha os melhores clipes de para o seu conteúdo de forma facilitada.
-                ● Edite e legende de forma automática.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header>Para quem é destinado o serviço da Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                ● Rádios Web
-                ● TVs Online
-                ● Podcasts
-                ● Criadores de conteúdo
-                Se você faz parte de algum desses grupos, agende uma demonstração conosco!s
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header>Quais resultados posso esperar ao usar a Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                ● Edite clipes 10 x mais rápidos, utilizando a nossa Inteligência Artificial.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="4">
-              <Accordion.Header>Quanto custa os serviços da Autoclipper?</Accordion.Header>
-              <Accordion.Body>
-                Atualmente estamos em teste alpha, disponibilizamos a autoclipper apenas para parceiros qualificados. Faça a sua inscrição e participe da lista de usuários alpha.
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </section>
-      </Flex>
-    </main>
+    <Flex item={'center'} gaps={'24px'}>
+      <H1 Title='Preguntas Frecuentes' color='white' />
+      <section className='FAQ-conteiner'>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>¿Qué es Autoclipper y cómo puede ayudar en la entrega de contenido?</Accordion.Header>
+            <Accordion.Body>
+              Autoclipper es una solución que impulsa la entrega de contenido, permitiéndote crear una máquina de cortes automática para YouTube. Esto significa que puedes reutilizar horas de videos grabados, transformándolos en nuevos formatos rápidamente. También ayuda a expandir tu estrategia de contenido, siguiendo los pasos de los mayores canales de entretenimiento, noticias y educación. ¡Agenda una demostración para saber más!
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>¿Cuáles son los principales beneficios de usar Autoclipper?</Accordion.Header>
+            <Accordion.Body>
+              ● Elige los mejores clips para tu contenido de forma fácil.<br />
+              ● Edita y subtitula automáticamente.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>¿Para quién está destinado el servicio de Autoclipper?</Accordion.Header>
+            <Accordion.Body>
+              ● Radios Web<br />
+              ● TVs Online<br />
+              ● Podcasts<br />
+              ● Creadores de contenido<br />
+              Si formas parte de alguno de estos grupos, ¡agenda una demostración con nosotros!
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>¿Qué resultados puedo esperar al usar Autoclipper?</Accordion.Header>
+            <Accordion.Body>
+              ● Edita clips 10 veces más rápido, utilizando nuestra Inteligencia Artificial.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>¿Cuánto cuestan los servicios de Autoclipper?</Accordion.Header>
+            <Accordion.Body>
+              Actualmente estamos en prueba alpha, y ofrecemos Autoclipper solo a socios calificados. Inscríbete y únete a la lista de usuarios alpha.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </Flex>
+  </main>
+  
   );
 }
 
